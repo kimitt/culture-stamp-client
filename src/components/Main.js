baa4094e90e8ebe7b737e8d270e7c658c0678f92
Sumin <sumin.kim.dev@gmail.com>
2022-10-18 00:36:49 +0900

    Design: Main 페이지 CSS 추가

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,7 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+// 임시 이미지 import 
 import Img1 from '../assets/images/1.jpg';
 import Img2 from '../assets/images/2.jpg';
 import Img3 from '../assets/images/3.jpg';
@@ -8,31 +12,94 @@ import Img7 from '../assets/images/7.jpg';
 import Img8 from '../assets/images/8.jpg';
 import Img9 from '../assets/images/9.jpg';
 
+const MainBackground = styled.div`
+  width:100vw;
+  height: 100%;
+  margin-left: calc(-50vw + 50%);
+  text-align: center;
+  background: #e5e7eb;
+`;
+
+const Head = styled.header`
+  padding-top: 13vh;
+`
+
+const Title = styled.span`
+  font-size: 17px;
+  font-weight: 600;
+`
+const Menu = styled.nav`
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  margin: 50px 0 100px 0;
+`;
+
+const MenuList = styled(Link)`
+  margin: 0 15px;
+  color: black;
+  font-size: 11px;
+  text-decoration: none;
+`
+const MainSection = styled.section` 
+  padding: 0 30px;
+`
+
+const Button = styled.span`
+  margin-left: 700px;
+  font-size: 0px;
+`
+
+const Thumbnail = styled.img`
+  width: 250px;
+  height: 250px;
+  margin: 10px;
+`
+
+const AddButton = styled.span`
+  margin-left: 80%;
+  font-size: 32px;
+`
+
+const Category = styled.div`
+  max-width: 93.5rem;
+  margin: 0 auto;
+  // grid : https://codepen.io/GeorgePark/pen/VXrwOP 보기 
+`
+
 function Main() {
   return (
-    <>
-      <header>
-        <div>C U L T U R E S T A M P</div>
-        <nav>
-          <li>CATEGORY</li>
-          <li>DATE</li>
-          <li>TODO</li>
-          <li>MYPAGE</li>
-          <li>LOGIN</li>
-        </nav>
-      </header>
-      <section>
-        <img src={Img1} />
-        <img src={Img2} />
-        <img src={Img3} />
-        <img src={Img4} />
-        <img src={Img5} />
-        <img src={Img6} />
-        <img src={Img7} />
-        <img src={Img8} />
-        <img src={Img9} />
-      </section>
-    </>
+    <MainBackground>
+      <Head>
+        <Title>C U L T U R E S T A M P</Title>
+        <Menu>
+          <MenuList to="/category">CATEGORY</MenuList>
+          <MenuList to="/date">DATE</MenuList>
+          <MenuList to="/todo">TODO</MenuList>
+          <MenuList to="/my-page">MYPAGE</MenuList>
+          <MenuList to="/login">LOGIN</MenuList>
+        </Menu>
+        <div>
+          <AddButton>+</AddButton>
+        </div>
+      </Head>
+      <MainSection>
+        <div>
+          <Button>+</Button>
+        </div>
+        <Category>
+          <Thumbnail src={Img1} alt="image1"/>
+          <Thumbnail src={Img2} alt="image2"/>
+          <Thumbnail src={Img3} alt="image3"/>
+          <Thumbnail src={Img4} alt="image4"/>
+          <Thumbnail src={Img5} alt="image5"/>
+          <Thumbnail src={Img6} alt="image6"/>
+          <Thumbnail src={Img7} alt="image7"/>
+          <Thumbnail src={Img8} alt="image8"/>
+          <Thumbnail src={Img9} alt="image9"/>
+        </Category>
+      </MainSection>
+    </MainBackground>
   );
 }
 
