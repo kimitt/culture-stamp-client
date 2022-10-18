fc9a0d8dfe55b8dfa5d54324fb32152d11a934e6
5Suminsss2 <sumin.kim.dev@gmail.com>
2022-10-18 13:02:14 +0900

    Design: thumbnail section 디자인

--- src/components/Main.js
+++ src/components/Main.js
@@ -42,7 +42,9 @@ const MenuList = styled(Link)`
   text-decoration: none;
 `
 const MainSection = styled.section` 
-  padding: 0 30px;
+  max-width: 93.5rem;
+  margin: 0 auto;
+  padding: 0 2rem;
 `
 
 const Button = styled.span`
@@ -50,10 +52,18 @@ const Button = styled.span`
   font-size: 0px;
 `
 
-const Thumbnail = styled.img`
-  width: 250px;
-  height: 250px;
-  margin: 10px;
+const ThumbnailItem = styled.div`
+  position: relative;
+  flex: 1 0 22rem;
+  margin: 1rem;
+  color: #fff;
+  cursor: pointer;
+`
+
+const ThumbnailImage = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 `
 
 const AddButton = styled.span`
@@ -62,9 +72,17 @@ const AddButton = styled.span`
 `
 
 const Category = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: -1rem -1rem;
+  padding-bottom: 3rem;
+`
+
+const Container = styled.div`
   max-width: 93.5rem;
   margin: 0 auto;
-  // grid : https://codepen.io/GeorgePark/pen/VXrwOP 보기 
+  padding: 0 2rem;
+
 `
 
 function Main() {
@@ -87,17 +105,37 @@ function Main() {
         <div>
           <Button>+</Button>
         </div>
-        <Category>
-          <Thumbnail src={Img1} alt="image1"/>
-          <Thumbnail src={Img2} alt="image2"/>
-          <Thumbnail src={Img3} alt="image3"/>
-          <Thumbnail src={Img4} alt="image4"/>
-          <Thumbnail src={Img5} alt="image5"/>
-          <Thumbnail src={Img6} alt="image6"/>
-          <Thumbnail src={Img7} alt="image7"/>
-          <Thumbnail src={Img8} alt="image8"/>
-          <Thumbnail src={Img9} alt="image9"/>
-        </Category>
+        <Container>
+          <Category>
+            <ThumbnailItem>
+              <ThumbnailImage src={Img1} alt="image1"/>
+            </ThumbnailItem>
+            <ThumbnailItem>
+              <ThumbnailImage src={Img2} alt="image2"/>
+            </ThumbnailItem>
+            <ThumbnailItem>
+              <ThumbnailImage src={Img3} alt="image3"/>
+            </ThumbnailItem>
+            <ThumbnailItem>
+              <ThumbnailImage src={Img4} alt="image4"/>
+            </ThumbnailItem>
+            <ThumbnailItem>
+              <ThumbnailImage src={Img5} alt="image5"/>
+            </ThumbnailItem>
+            <ThumbnailItem>
+              <ThumbnailImage src={Img6} alt="image6"/>
+            </ThumbnailItem>
+            <ThumbnailItem>
+              <ThumbnailImage src={Img7} alt="image7"/>
+            </ThumbnailItem>
+            <ThumbnailItem>
+              <ThumbnailImage src={Img8} alt="image8"/>
+            </ThumbnailItem>
+            <ThumbnailItem>
+              <ThumbnailImage src={Img9} alt="image9"/>
+            </ThumbnailItem>
+          </Category>
+        </Container>
       </MainSection>
     </MainBackground>
   );
