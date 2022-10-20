1730df0d0b73932af8ab61b3d90c0a06d36160cb
5Suminsss2 <sumin.kim.dev@gmail.com>
2022-10-20 12:35:04 +0900

    Design: 스크롤 감지 시 헤더 패딩 조절

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Main from './components/Main';
-import Test from './components/Test';
 import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/test" element={<Test />} />
       </Routes>
       <GlobalStyle />
     </BrowserRouter>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,8 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-// 임시 이미지 import 
+// 임시 이미지 import
 import Img1 from '../assets/images/1.jpg';
 import Img2 from '../assets/images/2.jpg';
 import Img3 from '../assets/images/3.jpg';
@@ -14,7 +15,7 @@ import Img9 from '../assets/images/9.jpg';
 
 // 전체 CSS 적용
 const MainBackground = styled.div`
-  width:100vw;
+  width: 100vw;
   height: 100%;
   margin-left: calc(-50vw + 50%);
   text-align: center;
@@ -25,25 +26,28 @@ const MainBackground = styled.div`
 const Head = styled.header`
   position: fixed;
   top: 0;
-  padding-top: 15vh;
+  padding-top: ${(props) => props.scrollActive || '15vh'};
   z-index: 999;
   background-color: #e5e7eb;
-`
+`;
 
 const Title = styled.span`
   width: 100vw;
   font-size: 17px;
   font-weight: 600;
-`
+`;
+
 // 카테고리 부분
 const Category = styled.div`
   display: none;
   /* justify-content: center; */
   margin-top: 10px;
-`
+`;
 const CategoryList = styled.div`
   margin-right: 15px;
-`
+`;
+
+// 메뉴 부분
 const Menu = styled.nav`
   display: flex;
   justify-content: center;
@@ -59,57 +63,46 @@ const MenuList = styled(Link)`
   &:hover {
     font-weight: 700;
   }
-  &:nth-child(1):hover{
+  &:nth-child(1):hover {
     margin-right: -30px;
-    
   }
-  &:nth-child(1):hover ${Category}{
+  &:nth-child(1):hover ${Category} {
     display: flex;
     font-weight: 500;
   }
-`
+`;
 
-// // 카테고리 부분
-// const Category = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   margin-bottom: 10px;
-// `
-// const CategoryList = styled.div`
-//   margin-right: 15px;
-// `
-
-// 카테고리 추가 버튼 부분
+// 글 추가 버튼 부분
 const ButtonSection = styled.div`
   display: flex;
   justify-content: center;
-`
+`;
 
 const AddButton = styled(Link)`
   margin-left: 80%;
   font-size: 32px;
   text-decoration: none;
   cursor: pointer;
-  &:hover{
+  &:hover {
     font-size: 34px;
   }
-`
+`;
 
 // 메인 부분
-const MainSection = styled.section` 
+const MainSection = styled.section`
   position: relative;
-  top: 280px;
+  top: 310px;
+  //top: 50px;
   max-width: 93.5rem;
   margin: 0 auto;
   padding: 0 2rem;
-`
+`;
 
 const Container = styled.div`
   max-width: 93.5rem;
   margin: 0 auto;
   padding: 0 2rem;
-
-`
+`;
 
 const Thumbnail = styled.div`
   display: flex;
@@ -118,12 +111,12 @@ const Thumbnail = styled.div`
   padding-bottom: 3rem;
 
   @supports (display: grid) {
-        display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(22rem, 1fr));
-        width: auto;
-        margin: 0;
-    } 
-`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(22rem, 1fr));
+    width: auto;
+    margin: 0;
+  }
+`;
 
 const ThumbnailItem = styled.div`
   position: relative;
@@ -131,34 +124,61 @@ const ThumbnailItem = styled.div`
   margin: 1rem;
   color: #fff;
   cursor: pointer;
-`
+`;
 
 const ThumbnailImage = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
-`
+`;
 
 function Main() {
+  const [scrollY, setScrollY] = useState(0);
+  const [scrollActive, setScrollActive] = useState(false);
+
+  // 스크롤 움직임 확인하는 함수
+  const scrollFixed = () => {
+    if (scrollY > 10) {
+      setScrollY(window.pageYOffset);
+      setScrollActive(true);
+    } else {
+      setScrollY(window.pageYOffset);
+      setScrollActive(false);
+    }
+  };
+
+  // 스크롤 감지 시 scrollFixed 함수 실행
+  useEffect(() => {
+    const scrollListener = () => {
+      window.addEventListener('scroll', scrollFixed);
+    };
+    scrollListener();
+    return () => {
+      window.removeEventListener('scroll', scrollFixed);
+    };
+  });
+
   return (
     <MainBackground>
-      <Head>
+      {/* 스크롤 감지 시 padding 조절 */}
+      <Head scrollActive={scrollActive ? '7vh' : ''}>
         <Title>C U L T U R E S T A M P</Title>
         <Menu>
-          <MenuList>CATEGORY
-          <Category className='category'>
-            <CategoryList>Movie</CategoryList>
-            <CategoryList>Memo</CategoryList>
-            <CategoryList>Book</CategoryList>
-            <CategoryList>Music</CategoryList>
-          </Category>
+          <MenuList>
+            CATEGORY
+            <Category className="category">
+              <CategoryList>Movie</CategoryList>
+              <CategoryList>Memo</CategoryList>
+              <CategoryList>Book</CategoryList>
+              <CategoryList>Music</CategoryList>
+            </Category>
           </MenuList>
           <MenuList to="/date">DATE</MenuList>
           <MenuList to="/todo">TODO</MenuList>
           <MenuList to="/my-page">MYPAGE</MenuList>
           <MenuList to="/login">LOGIN</MenuList>
         </Menu>
-        
+
         <ButtonSection>
           <AddButton>+</AddButton>
         </ButtonSection>
@@ -167,31 +187,31 @@ function Main() {
         <Container>
           <Thumbnail>
             <ThumbnailItem>
-              <ThumbnailImage src={Img1} alt="image1"/>
+              <ThumbnailImage src={Img1} alt="image1" />
             </ThumbnailItem>
             <ThumbnailItem>
-              <ThumbnailImage src={Img2} alt="image2"/>
+              <ThumbnailImage src={Img2} alt="image2" />
             </ThumbnailItem>
             <ThumbnailItem>
-              <ThumbnailImage src={Img3} alt="image3"/>
+              <ThumbnailImage src={Img3} alt="image3" />
             </ThumbnailItem>
             <ThumbnailItem>
-              <ThumbnailImage src={Img4} alt="image4"/>
+              <ThumbnailImage src={Img4} alt="image4" />
             </ThumbnailItem>
             <ThumbnailItem>
-              <ThumbnailImage src={Img5} alt="image5"/>
+              <ThumbnailImage src={Img5} alt="image5" />
             </ThumbnailItem>
             <ThumbnailItem>
-              <ThumbnailImage src={Img6} alt="image6"/>
+              <ThumbnailImage src={Img6} alt="image6" />
             </ThumbnailItem>
             <ThumbnailItem>
-              <ThumbnailImage src={Img7} alt="image7"/>
+              <ThumbnailImage src={Img7} alt="image7" />
             </ThumbnailItem>
             <ThumbnailItem>
-              <ThumbnailImage src={Img8} alt="image8"/>
+              <ThumbnailImage src={Img8} alt="image8" />
             </ThumbnailItem>
             <ThumbnailItem>
-              <ThumbnailImage src={Img9} alt="image9"/>
+              <ThumbnailImage src={Img9} alt="image9" />
             </ThumbnailItem>
           </Thumbnail>
         </Container>
